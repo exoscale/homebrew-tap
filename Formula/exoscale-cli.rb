953ca4d0c9639759e050884ea03a2360ce50ca4a
devops-wien-user <110428682+devops-wien-user@users.noreply.github.com>
2022-08-29 01:16:09 +0200

    Update exoscale-cli.rb
    fix bin.install

--- Formula/exoscale-cli.rb
+++ Formula/exoscale-cli.rb
@@ -12,7 +12,7 @@ class ExoscaleCli < Formula
     sha256 "7d2751a20d378be69a5a835e0254df9a1f17f6139bc9fe0b3638518d1df63aa6"
 
     def install
-      bin.install "exoscale-cli" => "exo"
+      bin.install "exo"
       man1.install Dir["manpage/exo*.1"]
       bash_completion.install "contrib/completion/bash/exo"
       zsh_completion.install "contrib/completion/zsh/_exo"
@@ -25,7 +25,7 @@ class ExoscaleCli < Formula
       sha256 "3326613a19794506bdb4e3107a92df0a3ee148691ceb09c64a0c2d1b1db91929"
 
       def install
-        bin.install "exoscale-cli" => "exo"
+        bin.install "exo"
         man1.install Dir["manpage/exo*.1"]
         bash_completion.install "contrib/completion/bash/exo"
         zsh_completion.install "contrib/completion/zsh/_exo"
@@ -36,7 +36,7 @@ class ExoscaleCli < Formula
       sha256 "c423fa76c8de4c4e2d85b73e7fb6eeb8bc85f8c44b9eb11138cc152ad6a5fa0d"
 
       def install
-        bin.install "exoscale-cli" => "exo"
+        bin.install "exo"
         man1.install Dir["manpage/exo*.1"]
         bash_completion.install "contrib/completion/bash/exo"
         zsh_completion.install "contrib/completion/zsh/_exo"
@@ -47,7 +47,7 @@ class ExoscaleCli < Formula
       sha256 "fd4060e5d6d86643bba9d8836da17683a65d62409679931e49f12376d4022e2b"
 
       def install
-        bin.install "exoscale-cli" => "exo"
+        bin.install "exo"
         man1.install Dir["manpage/exo*.1"]
         bash_completion.install "contrib/completion/bash/exo"
         zsh_completion.install "contrib/completion/zsh/_exo"
